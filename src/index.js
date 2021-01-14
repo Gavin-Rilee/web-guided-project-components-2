7228f86deb7514756f790856570608f1fff3f3f5
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2021-01-14 11:30:13 -0600

    wip

--- src/index.js
+++ src/index.js
@@ -5,11 +5,15 @@ import axios from "axios";
 const result = axios.get("https://lambda-times-api.herokuapp.com/friends");
 // console.log(result);
 
+console.log("1. about to fetch the data");
+
 result.then((futureData) => {
   // future code for when the data actually arrives
   // freedom to assume that the data is here
-  console.log(futureData);
+  console.log("2. here is the future data", futureData);
 });
+
+console.log("3. we requested the data with axios");
 // axios responds with JSON data
 // return value of axios is a Promise - representation of a future value
 
